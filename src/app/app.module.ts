b46b1dc875b2d685173e9073e0fcb7d302ddbb86
Aron <aron886tw@gmail.com>
2020-08-21 00:43:24 +0800

    調整

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule,
