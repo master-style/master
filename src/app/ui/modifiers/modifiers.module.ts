fe7133cc94a4e3887e215415b486c4f974e260fa
Aron <aron886tw@gmail.com>
2020-08-21 19:45:19 +0800

    調整

--- src/app/ui/modifiers/modifiers.module.ts
+++ src/app/ui/modifiers/modifiers.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AreaComponent } from './area/area.component';
+import { BackgroundComponent } from './background/background.component';
+import { BorderComponent } from './border/border.component';
+import { FontComponent } from './font/font.component';
+import { HoverComponent } from './hover/hover.component';
+import { MarginComponent } from './margin/margin.component';
+import { PaddingComponent } from './padding/padding.component';
+import { RadiusComponent } from './radius/radius.component';
+import { ScaleComponent } from './scale/scale.component';
+import { ShadowComponent } from './shadow/shadow.component';
+import { TextComponent } from './text/text.component';
+import { ZIndexComponent } from './z-index/z-index.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AreaComponent, BackgroundComponent, BorderComponent, FontComponent, HoverComponent, MarginComponent, PaddingComponent, RadiusComponent, ScaleComponent, ShadowComponent, TextComponent, ZIndexComponent],
   imports: [
     CommonModule
   ]
