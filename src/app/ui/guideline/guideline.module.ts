407555c07cc9e0de5ab88f7fc2b5b0cf5e8806af
Aron <aron886tw@gmail.com>
2020-08-21 15:39:23 +0800

    調整

---
+++ src/app/ui/guideline/guideline.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InstallationComponent } from './installation/installation.component';
+
+
+
+@NgModule({
+  declarations: [InstallationComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GuidelineModule { }
