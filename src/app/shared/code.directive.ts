56d093511bc65061628656a106b6b3e74f1f8ae8
Aron <aron886tw@gmail.com>
2020-08-30 15:13:29 +0800

    調整

--- src/app/shared/code.directive.ts
+++ src/app/shared/code.directive.ts
@@ -1,14 +1,16 @@
 import { Directive, ElementRef, Input } from '@angular/core';
 
 import Prism from 'prismjs';
-import loadLanguages from 'prismjs/components/';
+import 'prismjs/components/prism-scss.min.js';
+import 'prismjs/components/prism-bash.min.js';
+import 'prismjs/components/prism-typescript.min.js';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.min.js';
 
-// loadLanguages(['html', 'typescript', 'css', 'scss', 'bash']);
+import prettier from 'prettier';
+import htmlParser from 'prettier/parser-html.js';
+import typescriptParser from 'prettier/parser-typescript.js';
 
-console.log(loadLanguages);
-
-// import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.min.js';
-// import 'prismjs/plugins/line-numbers/prism-line-numbers.min.js';
+const prettierPlugins = [htmlParser, typescriptParser];
 
 const isDefined = (value: any): boolean => value !== undefined;
 
@@ -29,12 +31,12 @@ export class CodeDirective {
     ngOnInit(): void {
         const element = this.elementRef.nativeElement;
         const isTemplate = element.tagName === 'TEMPLATE';
-        let resultHTML: string;
+        let code: string;
         switch (this.codeLang) {
             case 'html':
                 if (isTemplate && isDefined(this.demo)) {
                     element.before(
-                        $('div', { class: 'demo' },
+                        $('div', { class: 'demo with:code' },
                             $('div', { class: 'demo-body' },
                                 ...element.children
                                     .map((eachChild) => eachChild.cloneNode(true))
@@ -42,15 +44,22 @@ export class CodeDirective {
                         )
                     );
                 }
-                resultHTML = Prism.highlight(element.outerHTML, Prism.languages.html, 'html');
+                code = $('div', {}, ...element.children).innerHTML;
+                code = prettier.format(code, {
+                    parser: 'html',
+                    plugins: prettierPlugins
+                });
                 break;
             default:
                 break;
         }
+
+        code = Prism.highlight(code, Prism.languages.html, 'html');
+
         if (isTemplate)
             element.before(
                 $('pre', {},
-                    $('code', { class: 'language-' + this.codeLang }).html(resultHTML)
+                    $('code', { class: 'language-' + this.codeLang }).html(code)
                 )
             );
     }
