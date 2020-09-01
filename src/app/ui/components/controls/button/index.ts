52479b61faaff89b9cb8b6d37373cc3064fc86f2
Aron <aron886tw@gmail.com>
2020-09-01 22:36:48 +0800

    調整

--- src/app/ui/components/controls/button/index.ts
+++ src/app/ui/components/controls/button/index.ts
@@ -5,23 +5,21 @@ import css from './index.scss';
 @Element('m-button')
 export class MasterButton extends HTMLElement {
 
-    @Attr() disabled = false;
+    @Attr() disabled = true;
     @Attr() loading = false;
 
+    buttonElement = $('button', {},
+        $('slot', {})
+    );
 
     constructor() {
         super();
         attachShadow(this, css)
-            .appendChild(
-                $('button', {},
-                    $('slot', {})
-                )
-            );
+            .appendChild(this.buttonElement);
     }
 
     protected static disabled(element, value, oldValue) {
-        console.log(value, oldValue);
-        console.log(element);
+        console.log(element.isConnected, value, oldValue);
     }
 
 }
