36009967826905290d6c78a4f2e137d1a71917ec
Aron <aron886tw@gmail.com>
2020-09-02 20:06:47 +0800

    調整

--- src/app/ui/components/controls/button/index.ts
+++ src/app/ui/components/controls/button/index.ts
@@ -1,16 +1,13 @@
 import { Element, Attr, attachShadow } from '@element';
-
 import css from './index.scss';
 
 @Element('m-button')
 export class MasterButtonElement extends HTMLElement {
 
-    // element
     shadow = $('button', {},
         $('slot', {})
     );
 
-    // attribute
     @Attr({ toggle: true, shadow: true }) disabled: boolean;
     @Attr({ toggle: true }) loading: boolean;
 
