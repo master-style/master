56d14c69937b712bdc4a9469188d4e3a27609db3
Aron <aron886tw@gmail.com>
2020-09-03 13:22:30 +0800

    調整

--- src/app/shared/prop/prop.component.ts
+++ src/app/shared/prop/prop.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'doc-prop',
-  templateUrl: './prop.component.html',
-  styleUrls: ['./prop.component.scss']
+    selector: 'doc-prop',
+    templateUrl: './prop.component.html',
+    styleUrls: ['./prop.component.scss']
 })
 export class PropComponent implements OnInit {
 
-  constructor() { }
+    @Input() types: [];
+    @Input() attr: string;
+    @Input() description: string;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+        console.log(this.description);
+    }
+
+    getTypeIcon(value) {
+        switch (value) {
+            case 'boolean':
+                return 'toggle-outline';
+            case 'string':
+                return 'text-outline';
+            case 'object':
+                return 'cube-outline';
+            case 'function':
+                return 'repeat-outline';
+            case 'number':
+                return 'calculator-outline';
+            case 'array':
+                return 'list-outline';
+            default:
+                return 'text-outline';
+        }
+    }
+
+    getTypeString(value) {
+        switch (value) {
+            case 'boolean':
+            case 'string':
+            case 'object':
+            case 'function':
+            case 'array':
+            case 'number':
+                return value;
+            default:
+                return "'" + value + "'";
+        }
+    }
 
 }
