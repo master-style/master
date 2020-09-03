8eb25245262b212a4847885084a651246da1d2f8
Aron <aron886tw@gmail.com>
2020-09-03 11:25:29 +0800

    調整

---
+++ src/app/shared/prop/prop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'doc-prop',
+  templateUrl: './prop.component.html',
+  styleUrls: ['./prop.component.scss']
+})
+export class PropComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeDirective } from './code.directive';
 import { TranslateModule } from '@ngx-translate/core';
+import { PropComponent } from './prop/prop.component';
 
 @NgModule({
-    declarations: [CodeDirective],
+    declarations: [CodeDirective, PropComponent],
     imports: [
         CommonModule,
         TranslateModule
@@ -12,7 +13,11 @@ import { TranslateModule } from '@ngx-translate/core';
     exports: [
         CommonModule,
         CodeDirective,
-        TranslateModule
+        TranslateModule,
+        PropComponent
+    ],
+    schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
     ]
 })
 export class SharedModule { }
