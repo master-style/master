005bb0e076c92b522ae89f886956c13e77c618a2
Aron <aron886tw@gmail.com>
2020-09-15 16:12:47 +0800

    調整

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { uiRoutes } from './ui/ui-routing.module';
 import { domRoutes } from './dom/dom-routing.module';
 import { AppService } from './app.service';
 import { elementRoutes } from './element/element-routing.module';
+import throttle from 'lodash/throttle';
 
 @Component({
     selector: 'doc-root',
@@ -21,7 +22,25 @@ export class AppComponent implements OnInit {
             ...this.resolvePaths(['element'], elementRoutes)
         );
     }
-    ngOnInit(): void { }
+
+    scrollDirection = 'up';
+
+    ngOnInit(): void {
+        setTimeout(() => {
+            let lastScrollTop = 0;
+            window.on('scroll', throttle(() => {
+                let st = window.pageYOffset || document.documentElement.scrollTop;
+                if (st > lastScrollTop) {
+                    // downscroll code
+                    this.scrollDirection = 'down';
+                } else {
+                    // upscroll code
+                    this.scrollDirection = 'up';
+                }
+                lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+            }, 100, { leading: false }), { passive: true });
+        }, 1000);
+    }
 
     resolvePaths(parentPaths, routes) {
         routes.map((eachRoute) => {
