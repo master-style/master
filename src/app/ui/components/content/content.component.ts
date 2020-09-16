970d775b3424db75a9a1aee6bf5839cc840b3ecb
Aron <aron886tw@gmail.com>
2020-09-16 14:06:12 +0800

    調整

--- src/app/ui/components/content/content.component.ts
+++ src/app/ui/components/content/content.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+    selector: 'app-content',
+    templateUrl: './content.component.html',
+    styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
-  ngOnInit(): void {
-  }
+    items;
+
+    ngOnInit(): void {
+        this.http.get('https://api.unsplash.com/photos/random/?client_id=jlEK9DMtCtnB7hg6E6Do3sGJwDnyNWocSs4m04oZDFk&count=30')
+            .subscribe((data) => {
+                this.items = data;
+                console.log(data);
+            });
+    }
 
 }
