bf0bb167a43346f2780502cf0ff96dccfe080931
Aron <aron886tw@gmail.com>
2020-09-18 21:53:51 +0800

    調整

--- src/app/ui/components/modal/index.ts
+++ src/app/ui/components/modal/index.ts
@@ -62,13 +62,24 @@ export class MasterModal extends MasterTogglable {
         const wrap = this.wrap;
 
         if (this.origin === 'trigger' && this.trigger) {
-            if (wrap.tagName === 'M-CONTENT') {
-                wrap.to({ x: 0, y: 0 }, 0);
+            const landing = this.querySelector('[part=landing]');
+            let closer: any = landing.parentElement;
+            do {
+                if (closer.tagName === 'M-CONTENT') {
+                    break;
+                } else {
+                    closer = closer.parentElement;
+                }
+            } while (closer.tagName === 'M-MODAL');
+
+            if (closer.tagName === 'M-CONTENT') {
+                console.log(closer);
+                closer.to({ x: 0, y: 0 }, 0);
             }
             const
                 callerRect = this.trigger.getBoundingClientRect(),
                 wrapRect = wrap.getBoundingClientRect(),
-                landingRect = this.querySelector('[part=landing]').getBoundingClientRect();
+                landingRect = landing.getBoundingClientRect();
             const scale = callerRect.width / landingRect.width;
             const x =
                 callerRect.left
@@ -83,66 +94,65 @@ export class MasterModal extends MasterTogglable {
                 - landingRect.top * scale / 2;
             keyframes = [
                 {
-                    transform: `translateX(${x + PX}) translateY(${y + PX}) scale${scale}`,
+                    transform: `translateX(${x + PX}) translateY(${y + PX}) scale(${scale})`,
                     height: callerRect.height / scale + landingRect.top + PX,
-                    opacity: this.fade && 0
+                    opacity: this.fade ? 0 : 1
                 },
                 {
                     transform: 'translateX(0) translateY(0) scale(1)',
                     height: wrapRect.height + PX,
-                    opacity: this.fade && 1
+                    opacity: 1
                 }
             ];
         } else {
-            let pushingKeyframes;
-            let dir;
-            let offset;
-            let pushingOffset;
-            switch (this.origin) {
-                case 'right':
-                    dir = 'X';
-                    offset = '100%';
-                    if (this.pushing)
-                        pushingOffset = -wrap.offsetWidth / 3;
-                    break;
-                case 'left':
-                    dir = 'X';
-                    offset = '-100%';
-                    if (this.pushing)
-                        pushingOffset = wrap.offsetWidth / 3;
-                    break;
-                case 'bottom':
-                    dir = 'Y';
-                    offset = '100%';
-                    if (this.pushing)
-                        pushingOffset = -wrap.offsetHeight / 3;
-                    break;
-                case 'top':
-                    dir = 'Y';
-                    offset = '-100%';
-                    if (this.pushing)
-                        pushingOffset = wrap.offsetHeight / 3;
-                    break;
-                case 'center':
-                    keyframes = [
-                        {
-                            transform: `scale(${this.hidden ? .9 : 1.1})`,
-                            opacity: 0
-                        },
-                        {
-                            transform: 'scale(1)',
-                            opacity: 1
-                        }
-                    ];
-                    break;
-                default:
-                    keyframes = [
-                        { opacity: 0 },
-                        { opacity: 1 }
-                    ];
-            }
+            if (!this.origin || this.origin === 'trigger') {
+                keyframes = [
+                    { opacity: 0 },
+                    { opacity: 1 }
+                ];
+            } else if (this.origin === 'center') {
+                keyframes = [
+                    {
+                        transform: `scale(${this.hidden ? .9 : 1.1})`,
+                        opacity: 0
+                    },
+                    {
+                        transform: 'scale(1)',
+                        opacity: 1
+                    }
+                ];
+            } else {
+                let pushingKeyframes;
+                let dir;
+                let offset;
+                let pushingOffset;
+                switch (this.origin) {
+                    case 'right':
+                        dir = 'X';
+                        offset = '100%';
+                        if (this.pushing)
+                            pushingOffset = -wrap.offsetWidth / 3;
+                        break;
+                    case 'left':
+                        dir = 'X';
+                        offset = '-100%';
+                        if (this.pushing)
+                            pushingOffset = wrap.offsetWidth / 3;
+                        break;
+                    case 'bottom':
+                        dir = 'Y';
+                        offset = '100%';
+                        if (this.pushing)
+                            pushingOffset = -wrap.offsetHeight / 3;
+                        break;
+                    case 'top':
+                        dir = 'Y';
+                        offset = '-100%';
+                        if (this.pushing)
+                            pushingOffset = wrap.offsetHeight / 3;
+                        break;
+                }
 
-            if (this.origin && this.origin !== 'center') {
                 if (this.pushing) {
                     pushingKeyframes = [
                         { transform: 'translate' + dir + '(0)' },
@@ -168,6 +178,7 @@ export class MasterModal extends MasterTogglable {
                     { transform: 'translate' + dir + '(0)' }
                 ];
             }
+
         }
 
         const overlayKeyframes = [
