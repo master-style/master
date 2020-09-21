03edec253a6b27d8a5789366a62ca8528ebf4462
Aron <aron886tw@gmail.com>
2020-09-21 12:03:48 +0800

    調整

--- src/app/shared/code.directive.ts
+++ src/app/shared/code.directive.ts
@@ -70,8 +70,7 @@ export class CodeDirective {
                         .replace(/<?_[\S]*?="[\s\S]*?"/g, '')
                         .replace(/<!--bindings[^>]*-->/g, '')
                         .replace(/<\s*input[^>]*>(.*?)/g, '')
-                        .replace(/<?(empty)="[^"]*"/g, '')
-                    ;
+                        .replace(/<?(empty)="[^"]*"/g, '');
 
                 if (isTemplateTag && this.codeDemo) {
                     this.demoElement = $('div', { class: 'demo with:code' },
@@ -150,4 +149,16 @@ export class CodeDirective {
             this.demoElement.toggleAttr('collapsed', this.codeCollapsed);
     }
 
+}
+
+if (typeof Worker !== 'undefined') {
+    // Create a new
+    const worker = new Worker('./code.worker', { type: 'module' });
+    worker.onmessage = ({ data }) => {
+        console.log(`page got message: ${data}`);
+    };
+    worker.postMessage('hello');
+} else {
+    // Web Workers are not supported in this environment.
+    // You should add a fallback so that your program still executes correctly.
 }
