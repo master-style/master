f72cf61a7159de4716cd196459528d61e208a0d3
Aron <aron886tw@gmail.com>
2020-09-24 22:42:00 +0800

    調整

--- src/app/ui/form/select/index.ts
+++ src/app/ui/form/select/index.ts
@@ -126,41 +126,41 @@ export class SelectElement extends ControlElement {
 
         let originRect = { top: 0, height: 0 };
         let $origin;
-        if (originOption && !originOption.hidden) {
-            $origin = originOption.$wrap;
-            this.$selectWrap.to($origin, 0);
-            originRect = $origin.getBoundingClientRect();
-        }
-        const
-            selectRect = this.$container.getBoundingClientRect(),
-            wrapH = this.$selectWrap.offsetHeight,
-            wrapW = this.$selectWrap.offsetWidth,
-            windowH = $window.innerHeight,
-            windowW = $window.innerWidth,
-            originOffsetTop = originRect.top + originRect.height / 2;
-        let top = selectRect.top + ($origin ? selectRect.height / 2 : 0) - originOffsetTop;
-        let left = selectRect.left;
-        // exceed Y
-        let exceedY = 0;
-        if (top <= 5) {
-            exceedY = top - 5;
-            top = 5;
-        } else if (top + wrapH >= windowH - 5) {
-            exceedY = top + wrapH - windowH + 5;
-            top = windowH - wrapH - 5;
-        };
-        // exceed X
-        if (left <= 5) {
-            left = 5;
-        } else if (left + wrapW >= windowW - 5) {
-            left = windowW - wrapW - 5;
-        }
-        this.$selectWrap.css({
-            top,
-            left,
-            minWidth: selectRect.width,
-            transformOrigin: '0 ' + (originOffsetTop + exceedY) + 'px'
-        });
+        // if (originOption && !originOption.hidden) {
+        //     $origin = originOption.$wrap;
+        //     this.$selectWrap.to($origin, 0);
+        //     originRect = $origin.getBoundingClientRect();
+        // }
+        // const
+        //     selectRect = this.$container.getBoundingClientRect(),
+        //     wrapH = this.$selectWrap.offsetHeight,
+        //     wrapW = this.$selectWrap.offsetWidth,
+        //     windowH = $window.innerHeight,
+        //     windowW = $window.innerWidth,
+        //     originOffsetTop = originRect.top + originRect.height / 2;
+        // let top = selectRect.top + ($origin ? selectRect.height / 2 : 0) - originOffsetTop;
+        // let left = selectRect.left;
+        // // exceed Y
+        // let exceedY = 0;
+        // if (top <= 5) {
+        //     exceedY = top - 5;
+        //     top = 5;
+        // } else if (top + wrapH >= windowH - 5) {
+        //     exceedY = top + wrapH - windowH + 5;
+        //     top = windowH - wrapH - 5;
+        // };
+        // // exceed X
+        // if (left <= 5) {
+        //     left = 5;
+        // } else if (left + wrapW >= windowW - 5) {
+        //     left = windowW - wrapW - 5;
+        // }
+        // this.$selectWrap.css({
+        //     top,
+        //     left,
+        //     minWidth: selectRect.width,
+        //     transformOrigin: '0 ' + (originOffsetTop + exceedY) + 'px'
+        // });
     }
 
     onAdded() {
