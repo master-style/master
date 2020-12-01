01cb0b27f032664ef326d3a609932a8e51ff9041
Aron <aron886tw@gmail.com>
2020-12-01 19:32:13 +0800

    調整

--- src/app/ui/components/popup/index.ts
+++ src/app/ui/components/popup/index.ts
@@ -1,4 +1,6 @@
 import { Element, ToggleableElement } from '../../../element';
+import { createPopper } from '@popperjs/core';
+
 import css from './popup.scss';
 
 const NAME = 'popup';
@@ -8,13 +10,30 @@ const NAME = 'popup';
     css
 })
 export class PopupElement extends ToggleableElement {
+    root;
+    trigger: HTMLElement;
+
     template = window['Master'](() => [
         'div', {
             part: 'root',
             $created: (element: HTMLElement) => this.root = element
         }, [
+            'slot'
         ]
     ]);
+
+    onOpen() {
+        createPopper(this.trigger, this, {
+            modifiers: [
+                {
+                    name: 'offset',
+                    options: {
+                        offset: [0, 8],
+                    },
+                },
+            ],
+        });
+    }
 
     render() {
         this.template.render(this.shadowRoot);
