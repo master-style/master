140d984ee6ac50df3bfb6de62fb87d76ecb75145
Aron <aron886tw@gmail.com>
2021-02-19 20:53:44 +0800

    fix: indicators

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './index.scss';
 
 export * from './data-renders';
 export * from './interactors';
+export * from './indicators';
 export * from './navigators';
 export * from './layout';
 export * from './icon';
