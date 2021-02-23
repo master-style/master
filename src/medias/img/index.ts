1f3119323701ec7edb1101a91ae951ad9c4515b1
Aron <aron886tw@gmail.com>
2021-02-23 11:50:27 +0800

    update img

--- src/medias/img/index.ts
+++ src/medias/img/index.ts
@@ -1,16 +1,36 @@
-import { Attr, Element } from '@master/element';
+import { Attr, Element, MasterElement } from '@master/element';
+import { Template } from '@master/template';
 import css from './img.scss';
 
 const NAME = 'img';
 
-
 @Element('m-' + NAME)
-export class ImgElement extends HTMLElement {
+export class ImgElement extends MasterElement {
     static css = css;
+
+    template = new Template(() => [
+        'img', {
+            part: 'master',
+            src: this.src,
+            srcset: this.srcset,
+            width: this.width,
+            height: this.height
+        }
+    ]);
 
     @Attr()
     src: string;
 
     @Attr()
     srcset: string;
+
+    @Attr()
+    width: number;
+
+    @Attr()
+    height: number;
+
+    render() {
+        this.template.render(this.shadowRoot);
+    }
 }
